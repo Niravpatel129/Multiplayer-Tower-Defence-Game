020dc70eab031adadbeb17feb56977c1b07dd00a
Niravpatel129 <niravpatelp129@gmail.com>
2019-08-23 09:21:39 -0400

    minor changes to client

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 // const bodyParser = require("    ");
 const path = require("path");
 const app = express();
-const port = 8080;
+const port = 5000;
 app.use(express.static(path.join(__dirname, "build")));
 
 app.get("/test", function(req, res) {
