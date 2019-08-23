2c9c2a8da0ee73b0c200703ce11950b23f1505c6
Niravpatel129 <niravpatelp129@gmail.com>
2019-08-23 09:10:41 -0400

    minor changes to server

--- server.js
+++ server.js
@@ -10,6 +10,15 @@ app.get("/test", function(req, res) {
   return res.send("pong");
 });
 
+if (process.env.NODE_ENV === "production") {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, "client/build")));
+  // Handle React routing, return all requests to React app
+  app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "client/build", "index.html"));
+  });
+}
+
 app.listen(process.env.PORT || port, function() {
   console.log("server running on port", port);
 });
